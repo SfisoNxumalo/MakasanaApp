a2eed3065c00e6991681e999cfc1981ad42785c2
Sfiso Mawila Nxumalo <sfisonxumalo69@gmail.com>
2023-11-03 14:32:15 +0200

    Business Dash UI update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessDashboardComponent } from './BusinessUI/business-dashboard/business-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component:BusinessDashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
