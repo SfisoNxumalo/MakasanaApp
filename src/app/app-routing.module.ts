1e42ebf368013f2d073d983b0665779fa9ce9e28
realebogamatlakala <real.matlakala96@gmail.com>
2023-11-03 15:11:15 +0200

    process

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { BusinessDashboardComponent } from './BusinessUI/business-dashboard/business-dashboard.component';
 import { LandingComponent } from './landing/landing.component';
 import { TrackOrderComponent } from './UserUI/track-order/track-order.component';
+import { OrderComponent } from './BusinessUI/order/order.component';
+import { ProcessComponent } from './BusinessUI/process/process.component';
 
 const routes: Routes = [
 
   {path:'',redirectTo:'landing-page', pathMatch: 'full' },
   { path: 'landing-page', component:LandingComponent},
   { path: 'track-page', component:TrackOrderComponent},
+  { path: 'orders', component:OrderComponent},
+  { path: 'process', component:ProcessComponent},
   {path: 'dashboard', component:BusinessDashboardComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { LandingNavBarComponent } from './landing-nav-bar/landing-nav-bar.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { BusinessDashboardComponent } from './BusinessUI/business-dashboard/business-dashboard.component';
 import { TrackOrderComponent } from './UserUI/track-order/track-order.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OrderComponent } from './order/order.component';
-import { ProcessComponent } from './process/process.component';
+import { OrderComponent } from './BusinessUI/order/order.component';
+import { ProcessComponent } from './BusinessUI/process/process.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LandingNavBarComponent,
+    BusinessDashboardComponent,
+    TrackOrderComponent,
+    OrderComponent,
+    ProcessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
