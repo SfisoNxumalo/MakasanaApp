11645409cddb25816c6fd582550a2dcde26acdc4
mfeke <mfekethothulane@gmail.com>
2023-11-03 11:35:39 +0200

    landing-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'landing-page', pathMatch: 'full' },
+  { path: 'landing-page', component:LandingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { LandingNavBarComponent } from './landing-nav-bar/landing-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LandingNavBarComponent
   ],
   imports: [
     BrowserModule,
