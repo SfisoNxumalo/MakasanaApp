b249a248937db88dce3416d2fc2c791fdcaf9cea
ncediswa32 <ncediswatabule.tabule399@gmail.com>
2023-11-07 14:02:05 +0200

    newupdates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { SlidebarComponent } from './slidebar/slidebar.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProfileComponent } from './profiles/profile/profile.component';
+import { SlidebarComponent } from './profiles/slidebar/slidebar.component';
+import { AboutUsComponent } from './profiles/about-us/about-us.component';
+import { ContactUsComponent } from './profiles/contact-us/contact-us.component';
 
 const routes: Routes = [
   { path: 'Profile-component', component: ProfileComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SlidebarComponent } from './slidebar/slidebar.component';
+import { ProfileComponent } from './profiles/profile/profile.component';
+import { SlidebarComponent } from './profiles/slidebar/slidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './profiles/about-us/about-us.component';
+import { ContactUsComponent } from './profiles/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +28,7 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     BrowserAnimationsModule,
     MatSlideToggleModule,
     MatSidenavModule,
-     
+
   ],
   providers: [],
   bootstrap: [AppComponent]
