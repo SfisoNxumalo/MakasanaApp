96906ba6052f2a7163dd0ea128d25ce6cffebd52
VickyDana <uviwedana09@gmail>
2023-11-07 13:53:20 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { OrderComponent } from './BusinessUI/order/order.component';
 import { ProcessComponent } from './BusinessUI/process/process.component';
 import { BusinessProductsComponent } from './BusinessUI/business-products/business-products.component';
 import { AddProductComponent } from './BusinessUI/add-product/add-product.component';
+import { ProductsComponent } from './User/products/products.component';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { AddProductComponent } from './BusinessUI/add-product/add-product.compon
     BusinessDashboardComponent,
     TrackOrderComponent,
     BusinessProductsComponent,
-    AddProductComponent
+    AddProductComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
