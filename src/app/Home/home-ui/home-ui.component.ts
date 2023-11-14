e9296186c53ca0bed75bf3ff7c066c20ff85856b
Sfiso Nxumalo <sfisonxumalo69@gmail.com>
2023-11-14 03:14:37 +0200

    updated

--- src/app/Home/home-ui/home-ui.component.ts
+++ src/app/Home/home-ui/home-ui.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-ui',
   templateUrl: './home-ui.component.html',
   styleUrls: ['./home-ui.component.css']
 })
-export class HomeUiComponent {
+export class HomeUiComponent implements OnInit{
+  foundUsers$!: Observable<any[]>;
+  private searchTerms = new Subject<string>();
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    // this.foundUsers$ = this.searchTerms.pipe(
+    //   // wait 300ms after each keystroke before considering the term
+    //   debounceTime(300),
+
+    //   // ignore new term if same as previous term
+    //   distinctUntilChanged(),
+
+    //   // switch to new search observable each time the term changes
+    //   switchMap((term: string) => this.mGetSearch(term)),
+    // );
+    const oo = [1, 2, 3, 5]
+    oo.forEach(i =>{
+      console.log(i);
+    })
+
+  }
+
+  mGetSearch(term:string):Observable<any>{
+
+    const oo = [1, 2, 3, 5]
+
+    const ssearchData = new BehaviorSubject<any>({});
+
+    // BehaviorSubject bh = new BehaviorSubject();
+
+    return ssearchData;
+  }
 
 }
--- src/app/Home/landing/landing.component.ts
+++ src/app/Home/landing/landing.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FakeStoreService } from 'src/app/services/fake-store.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
+  constructor(private fakeApi:FakeStoreService){}
+  image = "https://media.istockphoto.com/id/620737858/photo/cape-town-and-the-12-apostels-from-above.jpg?s=612x612&w=is&k=20&c=WBxI8OmAFXoGz5I5QjU0eI87I5C6K8h0Xs4JmArrEQQ=";
+
+
+  ngOnInit(): void {
+    this.mGet();
+  }
+  blLoadComplete = true;
+  products:any = [];
+
+  mGet(){
+    this.fakeApi.mGetFiveProducts().subscribe({
+      next: (res) => {(
+        this.products = res,
+        console.log(res),
+        this.blLoadComplete = false
+      )},
+      error: (err) => {
+        console.log(err)
+        this.blLoadComplete = false
+      }
+    })
+  }
 }
